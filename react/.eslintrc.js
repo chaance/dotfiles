efa6fb8a50b3bfc5831903b9582ca8bbf2e9d069
chance <chance@snapshotinteractive.com>
2018-07-17 01:11:35 -0500

    init

---
+++ react/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['prettier', 'prettier/react'],
+  env: {
+    browser: true,
+    es6: true,
+  },
+  plugins: ['react'],
+  globals: {
+    graphql: false,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true,
+      modules: true,
+    },
+  },
+};
