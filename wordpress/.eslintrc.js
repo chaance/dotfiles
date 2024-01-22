7238fe64531ef59ff7950ca32bf7af06864e63bd
Chance Strickland <hi@chance.dev>
2024-01-22 07:33:11 -0800

    rm wordpress files

--- wordpress/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	extends: [
-		// Remove if not using React
-		'@chancedigital/eslint-config/react',
-		'@chancedigital/eslint-config/jest',
-		'@chancedigital/eslint-config/wp',
-	],
-};
--- wordpress/stylelint.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	// https://github.com/chancedigital/stylelint-config
-	extends: [
-		'@chancedigital/stylelint-config/scss',
-		'@chancedigital/stylelint-config/wp',
-	],
-};
